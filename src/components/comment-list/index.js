f14ea60f5e44266f53fcfbc1541a166b6833af54
githorder <mygit.2018@gmail.com>
2023-12-25 15:22:11 +0500

    fix nesting comments when creating

--- src/components/comment-list/index.js
+++ src/components/comment-list/index.js
@@ -1,13 +1,41 @@
 import PropTypes from "prop-types";
 
+import treeToList from "../../utils/tree-to-list";
+import listToTree from "../../utils/list-to-tree";
+
 import "./style.css";
+
+function transformCommentList(list) {
+  let lev = 1;
+  return treeToList(listToTree(list), (item, level) => {
+    return {
+      name: item?.author?.profile?.name || item?.name,
+      dateCreate: item?.dateCreate,
+      isDeleted: item?.isDeleted,
+      _id: item?._id,
+      text: item?.text,
+      level,
+      parentId: item?.parent?._id,
+    };
+  }).reduce((arr, item) => {
+    if (item.dateCreate) {
+      lev = (item.level - 1) * 30 < 1024 - 550 ? item.level - 1 : 0;
+
+      const data = { ...item, level: lev };
+
+      return [...arr, data];
+    }
+
+    return arr;
+  }, []);
+}
 
 function CommentList({ list, renderItem }) {
   return (
     <>
       {Array.isArray(list) && list.length !== 0 && (
         <div className="CommentList">
-          {list.map((comment, i, arr) => {
+          {transformCommentList(list).map((comment, i, arr) => {
             return (
               <div
                 style={{
--- src/store-redux/comment/reducers.js
+++ src/store-redux/comment/reducers.js
@@ -1,34 +1,8 @@
-import treeToList from "../../utils/tree-to-list";
-import listToTree from "../../utils/list-to-tree";
-
 export const initialState = {
   data: {},
   waiting: false,
   waitToCreate: false,
 };
-
-function transformCommentList(list) {
-  let lev = 1;
-  return treeToList(listToTree(list), (item, level) => {
-    return {
-      name: item?.author?.profile?.name || item?.name,
-      dateCreate: item?.dateCreate,
-      isDeleted: item?.isDeleted,
-      _id: item?._id,
-      text: item?.text,
-      level,
-      parentId: item?.parent?._id,
-    };
-  }).filter((item) => {
-    if (item.dateCreate) {
-      lev = (item.level - 1) * 30 < 1024 - 550 ? item.level - 1 : 0;
-
-      const data = { ...item, level: lev };
-
-      return data;
-    }
-  });
-}
 
 function reducer(state = initialState, action) {
   switch (action.type) {
@@ -40,7 +14,7 @@ function reducer(state = initialState, action) {
         ...state,
         data: {
           ...action.payload,
-          list: transformCommentList(action.payload.list),
+          list: action.payload.list,
         },
         waiting: false,
       };
@@ -52,15 +26,14 @@ function reducer(state = initialState, action) {
       return { ...state, waitToCreate: true };
 
     case "comments/create-success":
-      console.log(state.data.list);
       return {
         ...state,
         data: {
           count: state.data.count + 1,
-          list: transformCommentList([
+          list: [
             ...state.data.list,
             { ...action.payload.comment, name: action.payload.user },
-          ]),
+          ],
         },
         waitToCreate: false,
       };
